8ee0d86f83ad0400777e67e23dba154616732a3d
Wako Fugicha <wakofugicha1@gmail.com>
2023-07-13 01:52:15 -0700

    Puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main (void)
+/**
+ * main - A program that prints a line using the puts function
+ * Return: 0 (success)
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
