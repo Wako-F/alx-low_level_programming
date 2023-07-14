ef81b147a25d2b86e26db8dbce88c38679e8b696
Wako Fugicha <wakofugicha1@gmail.com>
2023-07-14 08:08:29 -0700

    Print one to ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Function that prints digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
