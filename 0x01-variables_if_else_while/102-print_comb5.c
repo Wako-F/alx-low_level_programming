e829387eeed534189b64f6f9e5ef011960b09e93
Wako Fugicha <wakofugicha1@gmail.com>
2023-07-14 07:43:24 -0700

    Two two digit combos

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,15 +11,18 @@ for (i = 0; i < 100; i++)
 {
 	for (j = i; j < 100; j++)
 	{
-		putchar('0' + (i / 10));
-		putchar('0' + (i % 10));
-		putchar(' ');
-		putchar('0' + (j / 10));
-		putchar('0' + (j % 10));
-		if (i != 99 || j != 99)
+		if (i != j)
 		{
-			putchar(',');
+			putchar('0' + (i / 10));
+			putchar('0' + (i % 10));
 			putchar(' ');
+			putchar('0' + (j / 10));
+			putchar('0' + (j % 10));
+			if (i != 99 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 }
