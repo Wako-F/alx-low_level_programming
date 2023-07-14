821d5844b62403762dae6ecb1f65af26f302f352
Wako Fugicha <wakofugicha1@gmail.com>
2023-07-14 06:32:53 -0700

    Print all single digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Function that prints single digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+	putchar(i + '0');
+}
+putchar('\n');
+return (0);
+}
