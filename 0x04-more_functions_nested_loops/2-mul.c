83ca1fd5cae624063ab3e31a448648e61fbf8d81
Wako Fugicha <wakofugicha1@gmail.com>
2023-07-20 07:10:26 -0700

    Multiply two digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two digits
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the multiplication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
