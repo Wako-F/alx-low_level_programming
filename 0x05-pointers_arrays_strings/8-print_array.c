15006cf108229d3f9b53b3e99403e4fa1686a43a
Wako Fugicha <wakofugicha1@gmail.com>
2023-07-24 10:37:45 -0700

    PRint array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - Function that prints n elemtns
+ * @a: array
+ * @n: number of elemtns
+ * Return a and n
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < (n - 1); i++)
+{
+	printf("%d, ", a[i]);
+}
+if (i == (n - 1))
+{
+	printf("%d", a[n - 1]);
+}
+printf("\n");
+}
