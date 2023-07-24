48db816567d3ab190d798606e803b4ffcbcffa49
Wako Fugicha <wakofugicha1@gmail.com>
2023-07-24 08:45:52 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - Function that swaps values of ints
+ * @a: int to swipe
+ * @b: int to swipe
+ */
+void swap_int(int *a, int *b)
+{
+int i;
+i = *a;
+*a = *b;
+*b = i;
+}
