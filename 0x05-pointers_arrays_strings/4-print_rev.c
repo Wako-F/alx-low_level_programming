f54957bd8f969548a668458421791972a10e7dad
Wako Fugicha <wakofugicha1@gmail.com>
2023-07-24 09:12:05 -0700

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - Function that prints str in reverse
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+int i = 0;
+int j;
+while (*s != '\0')
+{
+	i++;
+	s++;
+}
+s--;
+for (j = i; j > 0; j--)
+{
+	_putchar(*s);
+	s--;
+}
+_putchar('\n');
+}
