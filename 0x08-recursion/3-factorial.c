3a029f8c3382a48d04faae8e72251a0c0c490d31
Wako Fugicha <wakokunu@gmail.com>
2023-08-02 10:42:03 -0700

    Factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - Returns factorial of n
+ * @n: integer input
+ * Return: 0 (Success)
+ */
+int factorial(int n)
+{
+if (n < 0)
+	return (-1);
+if (n == 1 || n == 0)
+	return (1);
+else
+	return (n * factorial(n - 1));
+}
