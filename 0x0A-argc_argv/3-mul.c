26205b091ce6f367aaec30bea083d2696caae816
Wako Fugicha <wakokunu@gmail.com>
2023-08-04 10:42:01 -0700

    Multiply numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main- Function that multiplies two numbers
+ * @argc: no. of arguments
+ * @argv: String of arguments
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+int i, j;
+
+if ((argc - 1) != 2)
+{
+	printf("Error\n");
+	return (1);
+}
+i = atoi(argv[1]);
+j = atoi(argv[2]);
+
+printf("%d\n", (i * j));
+return (0);
+}
