ffd33c944c78be22eb6765c5074ff2abb062ab9e
Wako Fugicha <wakokunu@gmail.com>
2023-08-04 10:04:04 -0700

    Print program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Function that prints program name
+ * Return: 0 (Success)
+ * @argc: no. of arguments
+ * @argv: string of arguments
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
