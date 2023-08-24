dabc1b080a8e2e8a44caa5aa1a484fbda3e75b5c
Wako Fugicha <wakokunu@gmail.com>
2023-08-24 08:19:42 -0700

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - Funciton that prints linked list elems
+ * @h: pointer
+ * Return: no. of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t cnt = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		cnt++;
+	}
+	return (cnt);
+}
